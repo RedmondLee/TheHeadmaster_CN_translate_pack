cb1a0319ebf7c51c9aeea332abb1dc09fd80b70a
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2021-11-07 04:40:59 +0000

    Update

--- reverse_img/src/main.rs
+++ reverse_img/src/main.rs
@@ -11,30 +11,18 @@ fn splitext(filename: &str) -> Option<&str> {
         .and_then(OsStr::to_str)
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let enc_str: String = String::from("--enc");
-    let mut enc_flag = false;
-    if args.len() > 1 {
-        if enc_str.eq(&args[1]) {
-            enc_flag = true;
-        }
-    }
-
-    let watch_list: Vec<&str> = vec!["png","jpg","jpeg","bmp","webp","txt"];
-    let watch_header: Vec<[u8; 4]> = vec![
-        [255, 216, 255, 225],
-        [255, 216, 255, 224], 
-        [255, 216, 255, 219],
-        [82, 73, 70, 70],
-        [137, 80, 78, 71]
-    ];
-    let paths = fs::read_dir(".\\images").unwrap();
+fn treat_dir(
+    watch_list: &Vec<&str>,
+    watch_header: &Vec<[u8; 4]>,
+    enc_flag: bool,
+    s_dir_str: String,
+) {
+    let paths = fs::read_dir(s_dir_str).unwrap();
     for path in paths {
         let path_str = path.unwrap().path().into_os_string().into_string().unwrap();
-        let md = fs::metadata(path_str).unwrap();
-        println!({:?} , md);
+        let md = fs::metadata(&path_str).unwrap();
         if md.is_dir() {
+            treat_dir(watch_list, watch_header, enc_flag, path_str);
             continue;
         }
         let ext = splitext(&*path_str).unwrap().to_lowercase();
@@ -64,4 +52,34 @@ fn main() {
             }
         }
     }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let enc_str: String = String::from("--enc");
+    let dec_str: String = String::from("--dec");
+    let help_str: String = String::from("-h");
+    let mut enc_flag = false;
+    if args.len() > 1 {
+        if enc_str.eq(&args[1]) {
+            enc_flag = true;
+        } else if dec_str.eq(&args[1]) {
+            enc_flag = false;
+        } else if help_str.eq(&args[1]) {
+            println!("Help: todo.");
+            return ();
+        }
+    }
+
+    let watch_list: Vec<&str> = vec!["png","jpg","jpeg","bmp","webp","txt"];
+    let watch_header: Vec<[u8; 4]> = vec![
+        [255, 216, 255, 225],
+        [255, 216, 255, 224], 
+        [255, 216, 255, 219],
+        [82, 73, 70, 70],
+        [137, 80, 78, 71]
+    ];
+    
+    let s_dir_str = String::from(".\\images");
+    treat_dir(&watch_list, &watch_header, enc_flag, s_dir_str);
 }
